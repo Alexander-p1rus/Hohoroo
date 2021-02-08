70e356c42514cba985aeeedd92b9de956764a2f3
Your Name <you@example.com>
2021-02-08 07:22:10 +0300

    add 4 block and add accordrion

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.accordion-section').forEach((section) => {
+    section.addEventListener('click', (e) => {
+
+        const height = e.target.closest('.accordion-section').querySelector('.accordion-body>*').clientHeight
+        e.target.closest('.accordion-section').querySelector('.accordion-body').style.maxHeight = height + 'px'
+
+         console.log(e.target.closest('.accordion-section').classList.value)
+
+        if (e.target.closest('.accordion-section').classList.value==="accordion-section opened"){
+            e.target.closest('.accordion-section').classList.remove('opened')
+            e.target.closest('.accordion-section').querySelector('.accordion-body').style.maxHeight="0px"
+        }else{
+            e.target.closest('.accordion-section').classList.add('opened')
+            e.target.closest('.accordion-section').querySelector('.accordion-body').style.maxHeight = height + 40+'px'
+        }
+    })
+})
