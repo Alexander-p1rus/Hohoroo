5c16ffd4efb3e4274e7a6e12da8d34414f3b7b0f
Your Name <you@example.com>
2021-02-13 13:43:42 +0300

    add 2 block and sim slider

--- js/index.js
+++ js/index.js
@@ -21,3 +21,38 @@ document.querySelectorAll('.accordion-section').forEach((section) => {
 })
 
 
+let slideIndex=1
+
+showSlides=(n)=>{
+    let i;
+    let slides=document.querySelectorAll('.mySlides')
+    let dot=document.querySelectorAll('.dot')
+
+    if (n>slides.length){
+        slideIndex=1
+    }else if(n<1){
+        slideIndex=slides.length
+    }
+
+    slides.forEach((slide)=>{
+        slide.style.display='none'
+    })
+
+    dot.forEach((dot)=>{
+        dot.classList.remove('active')
+    })
+    slides[slideIndex-1].style.display="block"
+    dot[slideIndex-1].classList.add('active')
+}
+
+showSlides(slideIndex)
+
+plusSlides=(n)=>{
+    showSlides(slideIndex+=n)
+}
+
+currentSlide=(n)=>{
+    showSlides(slideIndex=n)
+}
+
+
