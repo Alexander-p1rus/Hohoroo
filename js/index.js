949e7ae88058e20732add673bd673fa75aa6f166
Nelazsky <nelazsky.vladimir@gmail.com>
2021-02-18 14:39:58 +0300

    semiready calc

--- js/index.js
+++ js/index.js
@@ -114,3 +114,131 @@ menuItem.forEach((item)=>{
         navigation.style.display='none'
     }
 })
+
+
+
+
+
+$( document ).ready(function() {
+    $(".accordion-header").on("click", function () {
+        $(this).find(".circle").toggleClass('active');
+    })
+
+});
+
+
+//(Количество проданных стаканов * цена) - (аренда: 30) = выручка в день
+// ((Количество стаканов * цена) - (Аренда : 30)) * 55%(маржинальность) = прибыль в день
+
+
+
+$(window).on('load', function() {
+
+
+
+
+    var design = $("input[name=level]:checked").val();
+    var cupQuantity = parseFloat($("input[name=cupQuantity]").val());
+    var cupPrice = parseFloat($("input[name=cupPrice]").val());
+    var rent = parseFloat($("input[name=rent]:checked").val());
+
+    $("#design-type span").html(design);
+    $("#copywriting-num span").html(cupQuantity);
+    $("#non-copywriting-num span").html(cupPrice);
+    $("#rent span").html(cupPrice);
+    $("#add-ons span").html(rent);
+
+    switch (design) {
+        case 'Standard':
+            var WebPrice = 1;
+            var PricePerPage_copy = 1;
+            var PricePerPage_noncopy = 1;
+            break;
+        case 'Personalized':
+            var WebPrice = 1;
+            var PricePerPage_copy = 1;
+            var PricePerPage_noncopy = 1;
+            break;
+        case 'Premium':
+            var WebPrice = 1;
+            var PricePerPage_copy = 1;
+            var PricePerPage_noncopy = 1;
+            break;
+        default:
+            var WebPrice = 1;
+    }
+    var total = WebPrice + (cupQuantity * PricePerPage_copy) + (cupPrice * PricePerPage_noncopy);
+    $("#amount").html((total).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "1,"));
+});
+$(document).on('input change', 'form.pricing-calculator :input', function() {
+    var design = $("input[name=level]:checked").val();
+    var cupQuantity = parseFloat($("input[name=cupQuantity]").val());
+    var cupPrice = parseFloat($("input[name=cupPrice]").val());
+    var rent = $("input[name=rent]:checked").val();
+    var ssl = $("input[name=ssl]:checked").val();
+    var ecommerce = $("input[name=ecommerce]:checked").val();
+
+    switch (ssl) {
+        case 'SSL':
+            var ssladdon = 100;
+            var sslchosen = "Yes";
+            break;
+        default:
+            var ssladdon = 0;
+            var sslchosen = "No";
+    }
+    switch (ecommerce) {
+        case 'Ecommerce':
+            var ecommerceaddon = 3000;
+            var ecommercechosen = "Yes";
+            break;
+        default:
+            var ecommerceaddon = 0;
+            var ecommercechosen = "No";
+    }
+
+    $("#design-type span").html(design);
+    $("#copywriting-num span").html(cupQuantity);
+    $("label[for=cupQuantity] span").html(cupQuantity);
+    $("#non-copywriting-num span").html(cupPrice);
+    $("label[for=cupPrice] span").html(cupPrice);
+    $("#rent span").html(rent);
+    $("#add-ons span").html(ssl);
+
+    switch (design) {
+        case 'Standard':
+            var WebPrice = 1;
+            var PricePerPage_copy = 1;
+            var PricePerPage_noncopy = 1;
+            break;
+        case 'Personalized':
+            var WebPrice = 1;
+            var PricePerPage_copy = 1;
+            var PricePerPage_noncopy = 1;
+            break;
+        case 'Premium':
+            var WebPrice = 1;
+            var PricePerPage_copy = 1;
+            var PricePerPage_noncopy = 1;
+            break;
+        default:
+            var WebPrice = 1;
+    }
+    switch (rent) {
+        case 'Офис':
+            var RentCost = 5000;
+            break;
+        case 'Магазин':
+            var RentCost = 8000;
+            break;
+        case 'Спорт':
+            var RentCost = 12000;
+            break;
+        default:
+            var RentCost = 5000;
+    }
+
+    var total = (WebPrice + (cupQuantity * PricePerPage_copy) + (cupPrice * PricePerPage_noncopy) + ssladdon + ecommerceaddon) - RentCost;
+    $("#amount").html((total).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "1,"));
+
+});
